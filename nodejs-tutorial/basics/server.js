f0e4c262d035c748b0e5fdc43279f1e664760316
fouad <Chamma.fouad@gmail.com>
2018-06-03 15:46:04 +1000

    Used fs to fetch and render html page

--- nodejs-tutorial/basics/server.js
+++ nodejs-tutorial/basics/server.js
@@ -1,9 +1,19 @@
 var http = require('http')
+var fs = require('fs')
 
 function onRquest(request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World!')
-  response.end()
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  fs.readFile('./index.html', null, function(error, data){
+    if(error){
+      response.writeHead(404)
+      response.write('File Not Found')
+    }
+
+    else {
+      response.write(data)
+    }
+    response.end()
+  })
 }
 
 http.createServer(onRquest).listen(2000)
