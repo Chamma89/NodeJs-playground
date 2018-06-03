42d3b5baf6588762ea31cd227214160103e09822
fouad <Chamma.fouad@gmail.com>
2018-06-03 16:39:38 +1000

    created routes to two separate HTML pages

--- nodejs-tutorial/basics/server.js
+++ nodejs-tutorial/basics/server.js
@@ -1,19 +1,20 @@
 var http = require('http')
 var fs = require('fs')
+var app = require('./app')
 
-function onRquest(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  fs.readFile('./index.html', null, function(error, data){
-    if(error){
-      response.writeHead(404)
-      response.write('File Not Found')
-    }
+// function onRquest(request, response){
+//   response.writeHead(200, {'Content-Type': 'text/html'})
+//   fs.readFile('./index.html', null, function(error, data){
+//     if(error){
+//       response.writeHead(404)
+//       response.write('File Not Found')
+//     }
 
-    else {
-      response.write(data)
-    }
-    response.end()
-  })
-}
+//     else {
+//       response.write(data)
+//     }
+//     response.end()
+//   })
+// }
 
-http.createServer(onRquest).listen(2000)
+http.createServer(app.handleRequest).listen(2000)
